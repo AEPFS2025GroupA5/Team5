c67729286235750217361c84fac4076a6eb4001f
Saliou79 <Saliousentd@gmail.com>
2025-03-24 11:33:14 +0100

    new

---
+++ database/adress.py
@@ -0,0 +1,12 @@
+
+## This file defines the Class "Adress"
+class Adress:
+    def __init__(self, street:str, city:str, zipcode:int):
+      
+        self.street = street
+        self.city = city
+        self.zipcode = zipcode
+
+
+
+        
