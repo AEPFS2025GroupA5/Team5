d0a091b479ab1a17b82588e2aaa731be681f1896
acmee <a.mueller020604@gmail.com>
2025-05-12 11:34:43 +0200

    git ignore

--- main.py
+++ main.py
@@ -15,9 +15,14 @@ os.environ["DB_FILE"] = os.path.abspath("database/hotel_reservation_sample.db")
 # V2 = data_access.RoomTypeDataAccess()
 V3 = data_access.RoomDataAccess()
 # V4 = data_access.HotelDataAccess()
+# V5 = data_access.GuestDataAccess()
+V6 = data_access.InvoiceDataAccess()
 
 # # # ##Test
 
 test = V3.read_all_rooms()
 print(test)
 
+inv = V6.read_all_invoice()
+for i in inv:
+    print(i)
