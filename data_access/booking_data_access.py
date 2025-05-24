a677f9967acb12730a10f96bd32e11515387b1eb
acme25 <a.mueller020604@gmail.com>
2025-05-24 21:23:18 +0200

    booking DAL

--- data_access/booking_data_access.py
+++ data_access/booking_data_access.py
@@ -16,17 +16,12 @@ class BookingDataAccess(BaseDataAccess):
         rows = self.fetchall(sql)
         all_bookings = []
 
-        
         for booking_id, room_id, check_in, check_out, total_amount, guest_id, cancelled in rows:
-            #check_in_date = datetime.strptime(str(check_in), "%Y-%m-%d").date()
-            #check_out_date = datetime.strptime(str(check_out), "%Y-%m-%d").date()
             is_cancelled = bool(cancelled)
             booking= model.Booking(booking_id, room_id, check_in, check_out, total_amount, guest_id, is_cancelled)
             all_bookings.append(booking)
 
         return all_bookings
+    
+    
 
-        # return [
-        #         model.Booking(booking_id, room_id, check_in_date, check_out_date, total_amount, guest_id, is_cancelled)
-                
-        # ]
