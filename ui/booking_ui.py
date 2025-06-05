94b61d5039d775ce4124b860dd510ebc1e0233fa
acme25 <a.mueller020604@gmail.com>
2025-06-05 13:53:28 +0200

    Userstory 2.2 UI steht mehr oder weniger

--- ui/booking_ui.py
+++ ui/booking_ui.py
@@ -39,3 +39,12 @@ def get_checkout_date_input() -> date:
             return ui.input_helper.input_valid_date("Enter check-out date (YYYY-MM-DD): ")
         except Exception as e:
             print(f"Invalid input: {e}")
+
+def get_userfriendly_room(room: model.Room) -> str:
+    roomman= business_logic.RoomManager()
+    hotelman= business_logic.HotelManager()
+
+    r = roomman.get_room_by_id(room.room_id)
+    h = hotelman.read_hotel_by_id(r.hotel_id)
+    return f"Hotel: {h.name},{h.stars}, {h.address.city}, Room number: {r.room_number}, Room description: {r.room_type.description}, Max Guests: {r.room_type.max_guests}, Price per night: CHF {r.price_per_night}"
+
