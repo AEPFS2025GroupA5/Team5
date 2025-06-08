5455d3899cae4eb98ea419bde0b6b54d5881fa4a
acme25 <a.mueller020604@gmail.com>
2025-06-08 19:14:37 +0200

    Invoice Manager

--- business_logic/invoice_manager.py
+++ business_logic/invoice_manager.py
@@ -7,7 +7,9 @@ from datetime import date
 class InvoiceManager:
     def __init__(self):
         self.__invoice_da = data_access.InvoiceDataAccess()
+        self.__booking_dao= data_access.BookingDataAccess()
     
+    #Read Functions
     def read_all_invoice(self) -> list[model.Invoice]:
         return self.__invoice_da.read_all_invoice()
     
@@ -16,11 +18,15 @@ class InvoiceManager:
         ) -> model.Invoice:
         return self.__invoice_da.read_invoice_by_id(invoice_id)
     
+    #Manipulate Data Functions
     def create_new_invoice(self,
                            booking_id: int,
                            issue_date: date,
                            total_amount: float
         ) -> model.Invoice:
+        booking= self.__booking_dao.read_booking_by_id(booking_id)
+        if booking is None:
+            raise ValueError("There is no such booking in our systems")
         
         return self.__invoice_da.create_new_invoice(booking_id, issue_date, total_amount)
 
@@ -28,9 +34,17 @@ class InvoiceManager:
                         invoice_id:int,
                         new_total_amount:float
         )-> None:
+        invoice= self.__invoice_da.read_invoice_by_id(invoice_id)
+        if invoice is None:
+            raise ValueError("There is no such Invoice in our systems")
+
         return self.__invoice_da.update_invoice_by_total_amount(invoice_id, new_total_amount)
 
     def delete_invoice_by_id(self, 
                         invoice_id:int
         ) -> None:
+        invoice= self.__invoice_da.read_invoice_by_id(invoice_id)
+        if invoice is None:
+            raise ValueError("There is no such Invoice in our systems")
+        
         return self.__invoice_da.delete_invoice_by_id(invoice_id)
