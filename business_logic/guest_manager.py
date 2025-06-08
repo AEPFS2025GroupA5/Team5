174d018cde72c81d462bd1613eeed9ad22c0c5dc
acme25 <a.mueller020604@gmail.com>
2025-06-08 19:52:39 +0200

    Guest Manager

--- business_logic/guest_manager.py
+++ business_logic/guest_manager.py
@@ -7,12 +7,18 @@ class GuestManager:
     def __init__(self):
         self.__guest_da = data_access.GuestDataAccess()
     
+    #Read Functions
     def read_all_guest(self) -> list[model.Guest]:
         return self.__guest_da.read_all_guest()
     
     def read_guest_by_id(self,
                             guest_id:int
         ) -> model.Guest:
+        guest= self.__guest_da.read_guest_by_id(guest_id)
+
+        if guest is None:
+            raise ValueError("There is no such guest in our systems")
+
         return self.__guest_da.read_guest_by_id(guest_id)
     
     def read_guest_by_name(self, 
@@ -20,17 +26,21 @@ class GuestManager:
         ) -> model.Guest:
         return self.__guest_da.read_guest_by_name(last_name)
 
+    #Manipulate Data Functions
     def create_new_guest(self,
                             first_name: str,
                             last_name: str,
                             email: str,
                             address_id: int
         ) -> model.Guest:
-
+        if "@" not in email or "." not in email:
+            raise ValueError("Email must be a valid email address.")
+    
         all_guests= self.read_all_guest()
         for guest in all_guests:
             if guest.email == email:
                 raise ValueError("This email is already in use.")
+            
             if (guest.first_name == first_name and
                 guest.last_name == last_name and
                 guest.address.address_id == address_id):
